003805f64b62ec9d7afb94e94e13acc4e7cc60c5
Vivek Kumar <118115653+VivekLpu@users.noreply.github.com>
2024-07-23 12:18:30 +0530

    Update Largest Sum Contiguous Subarray in Range.cpp
    Here is the solution of this problem..

--- Segment and Binary Indexed Trees/Largest Sum Contiguous Subarray in Range.cpp
+++ Segment and Binary Indexed Trees/Largest Sum Contiguous Subarray in Range.cpp
@@ -1,1 +1,24 @@
-//will be updated soon
+//Function to update a value in input array and segment tree.
+void update(int arr[], int arrSize, int index, int value)
+{
+    arr[index - 1] = value;  // Updating the value in the array
+    int n = arrSize;
+    for (int i = 0; i < n; ++i) {
+      
+    }
+}
+
+//Funciton to return the Maximum-Sum in the range.
+int query(int arr[], int n, int left, int right) 
+{
+    // Function to calculate the maximum sum subarray using Kadane's algorithm
+    int max_so_far = arr[left - 1];
+    int curr_max = arr[left - 1];
+
+    for (int i = left; i < right; i++) {
+        curr_max = max(arr[i], curr_max + arr[i]);
+        max_so_far = max(max_so_far, curr_max);
+    }
+
+    return max_so_far;
+}
